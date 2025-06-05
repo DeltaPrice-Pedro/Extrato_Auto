10f869ffc125858bab6e63248e0bfd8d99a050e7
Pedro Henrique Resende Menezes <pedro.menezes.1439318@sga.pucminas.br>
2025-06-05 11:56:31 -0300

    updt pag_bank.py

--- code/banks/pag_bank.py
+++ code/banks/pag_bank.py
@@ -1,5 +1,6 @@
 from bank import Bank
 from file import File
+from pandas import isna
 
 class PagBank(Bank):
     """
@@ -15,29 +16,37 @@ class PagBank(Bank):
         """
         qnt_pages = arquivo.lenght()
 
-        tabela1 = arquivo.custom_read(area_lida=[16,0,100,100], pg=1)
+        tabela1 = self.read_firt_page(arquivo)
         
         if qnt_pages > 1:
             arquivo_complt = arquivo.custom_read(pg=f'2-{qnt_pages}',\
-                area_lida=[0,0,100,100])
+                area_lida=[0,0,100,100], header= False)
             
-        arquivo_complt.insert(0,tabela1[0])
+        arquivo_complt.insert(0,tabela1)
 
-        self.filt_colunas(arquivo_complt, ["Data","", "Histórico","", "Valor"])
+        arquivo_complt = list(
+            map(lambda x: x.dropna(axis=1, how='all'), arquivo_complt)
+        )
+        self.filt_colunas(arquivo_complt, ["Data", "Histórico", "Valor"])
         self.inserir_espacos(troca=True)
         self.col_inf_sinal(6)
         self.__filtro_linhas()
 
         return self.df
+    
+    def read_firt_page(self, arquivo):
+        tabela1 = arquivo.custom_read(area_lida=[25,0,100,100], pg=1)[0]
+        return tabela1 if len(tabela1.columns) == 3 else \
+            arquivo.custom_read(area_lida=[30,0,100,100], pg=1)[0]
 
     def __filtro_linhas(self):
         """
         Ajusta linhas do extrato do PagBank.
         """
-        self.df.fillna('', inplace=True)
         for index, row in self.df.iterrows():
-            self.df.loc[[index], ['Data']] = row.Data[:10]
-            self.df.loc[[index], ['Histórico']] = str(row.Histórico).replace('A receber', '').replace('Disponível','')
+            if isna(row.Histórico):
+                self.df.loc[[index],['Histórico']] =\
+                f"{self.df['Histórico'].loc[index - 1]} {self.df['Histórico'].loc[index + 1]}"
 
-        self.df = self.df[self.df.Data != '']
-        self.df = self.df[self.df.Data.apply(lambda x: x[0].isnumeric())]
+        self.df.dropna(inplace=True)
+        self.df = self.df[self.df.Histórico != 'Saldo do dia']
